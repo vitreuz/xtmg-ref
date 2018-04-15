1b36907f8ead887c9974ffde371e51940c7a28bf
Sebastian Vidrio <svidrio@gmail.com>
2018-04-14 23:32:21 -0700

    add storybook support for web-ts

--- web-ts/src/index.tsx
+++ web-ts/src/index.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import XWingFont from './components/xwing_font';
-import { Action } from './client/Ship';
-import { FontType } from 'xwing_font/XWingFont';
+import { Action } from 'client/Ship';
+import { FontType } from 'components/xwing_font/XWingFont';
 
 ReactDOM.render(
   <XWingFont symbol={Action.Evade} type={FontType.action} />,
