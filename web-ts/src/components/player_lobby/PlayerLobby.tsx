349875b6c085efff5f761e6abfdbd10bbe36e200
Sebastian Vidrio <svidrio@gmail.com>
2018-04-29 14:27:09 -0700

    connect all the pieces in PlayerLobby

--- web-ts/src/components/player_lobby/PlayerLobby.tsx
+++ web-ts/src/components/player_lobby/PlayerLobby.tsx
@@ -5,11 +5,24 @@ import { Ship } from 'client/Ship';
 import PlayersList from '../players_list';
 import PlayerMenu from '../player_menu/index';
 import PlayerForm from '../player_form/index';
+import { PFState } from '../player_form/PlayerForm';
 
 interface PLProps {
   starterShips: Ship[];
+  CreatePlayer: (player: PFState) => string;
+  ListUpgradesForPlayer: (id: string) => Upgrade[];
+  FetchPlayer: (id: string) => Player;
+  UpdatePlayerHangarUpgrades: (
+    player_id: string,
+    upgrade_id: number,
+    action: string
+  ) => void;
+  UpdatePlayerSlots: (
+    player_id: string,
+    upgrade_id: number,
+    action: string
+  ) => void;
 }
-
 interface PLState {
   chosenPlayer?: Player;
   isCreateOpen: boolean;
@@ -26,48 +39,112 @@ class PlayerLobby extends React.Component<PLProps, PLState> {
       players: [],
       upgrades: []
     };
+
+    this.onCancelCreate = this.onCancelCreate.bind(this);
+    this.onCreatePlayer = this.onCreatePlayer.bind(this);
+    this.onEquipUpgrade = this.onEquipUpgrade.bind(this);
+    this.onNewPlayer = this.onNewPlayer.bind(this);
+    this.onPurchaseUpgrade = this.onPurchaseUpgrade.bind(this);
+    this.onSelectPlayer = this.onSelectPlayer.bind(this);
+    this.onUnequipUpgrade = this.onUnequipUpgrade.bind(this);
   }
 
-  startNewPlayer() {}
+  onCancelCreate(): void {
+    this.setState({ isCreateOpen: false });
+  }
+
+  onCreatePlayer(player: PFState): string {
+    const { CreatePlayer } = this.props;
+
+    return CreatePlayer(player);
+  }
+
+  onEquipUpgrade(id: number): void {
+    const { UpdatePlayerSlots } = this.props;
+
+    UpdatePlayerSlots(this.playerID(), id, 'add');
+    this.updatePlayerState(this.playerID());
+  }
+
+  onNewPlayer(): void {
+    this.setState({ isCreateOpen: true });
+  }
+
+  onPurchaseUpgrade(id: number): void {
+    const { UpdatePlayerHangarUpgrades } = this.props;
+
+    UpdatePlayerHangarUpgrades(this.playerID(), id, 'add');
+    this.updatePlayerState(this.playerID());
+  }
+
+  onSelectPlayer(id: string): void {
+    this.updatePlayerState(id);
+  }
+
+  onUnequipUpgrade(id: number): void {
+    const { UpdatePlayerSlots } = this.props;
+
+    UpdatePlayerSlots(this.playerID(), id, 'remove');
+    this.updatePlayerState(this.playerID());
+  }
+
+  updatePlayerState(id: string): void {
+    const { ListUpgradesForPlayer, FetchPlayer } = this.props;
+
+    const player = FetchPlayer(id);
+    const upgrades = ListUpgradesForPlayer(id);
+    this.setState({ chosenPlayer: player, upgrades: upgrades });
+  }
+
+  playerID(): string {
+    const { chosenPlayer } = this.state;
+    if (!chosenPlayer) {
+      throw 'cannot purchase upgrade without a chosen player';
+    }
+
+    return chosenPlayer.id;
+  }
+
+  renderLobby(props: PLProps, state: PLState): JSX.Element {
+    const { chosenPlayer, isCreateOpen, players, upgrades } = state;
+    const { starterShips } = props;
+
+    if (!!chosenPlayer) {
+      return (
+        <PlayerMenu
+          player={chosenPlayer}
+          upgrades={upgrades}
+          EquipUpgrade={this.onEquipUpgrade}
+          PurchaseUpgrade={this.onPurchaseUpgrade}
+          UnequipUpgrade={this.onUnequipUpgrade}
+        />
+      );
+    }
+    if (isCreateOpen) {
+      return (
+        <PlayerForm
+          starterShips={starterShips}
+          CreatePlayer={this.onCreatePlayer}
+          CancelForm={this.onCancelCreate}
+        />
+      );
+    }
+    return (
+      <PlayersList
+        players={players}
+        NewPlayer={this.onNewPlayer}
+        SelectPlayer={this.onSelectPlayer}
+      />
+    );
+  }
 
   render() {
     return (
-      <div className="player-lobby">{renderLobby(this.props, this.state)}</div>
+      <div className="player-lobby">
+        {this.renderLobby(this.props, this.state)}
+      </div>
     );
   }
-}
-
-function renderLobby(props: PLProps, state: PLState): JSX.Element {
-  const { chosenPlayer, isCreateOpen, players, upgrades } = state;
-  const { starterShips } = props;
-
-  if (!!chosenPlayer) {
-    return (
-      <PlayerMenu
-        player={chosenPlayer}
-        upgrades={upgrades}
-        EquipUpgrade={() => {}}
-        PurchaseUpgrade={() => {}}
-        UnequipUpgrade={() => {}}
-      />
-    );
-  }
-  if (isCreateOpen) {
-    return (
-      <PlayerForm
-        starterShips={starterShips}
-        CreatePlayer={() => {}}
-        CancelForm={() => {}}
-      />
-    );
-  }
-  return (
-    <PlayersList
-      players={players}
-      NewPlayer={() => {}}
-      SelectPlayer={() => {}}
-    />
-  );
 }
 
 export default PlayerLobby;
--- web-ts/src/components/player_lobby/__tests__/PlayerLobby.test.js
+++ web-ts/src/components/player_lobby/__tests__/PlayerLobby.test.js
@@ -5,8 +5,24 @@ import PlayersList from '../../players_list/index';
 import PlayerLobby from '../PlayerLobby';
 import PlayerForm from '../../player_form';
 import PlayerMenu from '../../player_menu/index';
+import { wrap } from 'module';
 
 describe('PlayerLobby', () => {
+  let fetchPlayer = jest.fn(id => ({ id: id }));
+  let listUpgrades = jest.fn(() => ['some-upgrade']);
+  let createPlayer = jest.fn();
+
+  let updatePlayerSlots = jest.fn();
+  let updatePlayerHangarUpgrades = jest.fn();
+
+  beforeEach(() => {
+    createPlayer.mockClear();
+    fetchPlayer.mockClear();
+    listUpgrades.mockClear();
+    updatePlayerSlots.mockClear();
+    updatePlayerHangarUpgrades.mockClear();
+  });
+
   describe('render', () => {
     describe('when no player is chosen and not creating a player', () => {
       it('renders a PlayersList', () => {
@@ -21,8 +37,8 @@ describe('PlayerLobby', () => {
       });
     });
 
-    describe('when no player is chosen and not creating a player', () => {
-      it('renders a PlayersList', () => {
+    describe('when no player is chosen and creating a player', () => {
+      it('renders a PlayersForm', () => {
         const wrapper = shallow(<PlayerLobby />);
         wrapper.setState({ chosenPlayer: undefined, isCreateOpen: true });
 
@@ -35,19 +51,295 @@ describe('PlayerLobby', () => {
     });
 
     describe('when a player is locked in', () => {
-      const wrapper = shallow(<PlayerLobby />);
-      wrapper.setState({ chosenPlayer: helpers.players.leeroyjenkins });
+      it('renders a PlayerMenu', () => {
+        const wrapper = shallow(<PlayerLobby />);
+        wrapper.setState({ chosenPlayer: helpers.players.leeroyjenkins });
 
-      expect(wrapper).toMatchElement(
-        <div>
-          <PlayerMenu />
-        </div>
-      );
+        expect(wrapper).toMatchElement(
+          <div>
+            <PlayerMenu />
+          </div>
+        );
+      });
     });
   });
 
-  describe('startNewPlayer', () => {});
-  describe('onNewPlayerCreate', () => {});
-  describe('onChoosePlayer', () => {});
-  describe('onUnchoosePlayer', () => {});
+  describe('state', () => {
+    describe('onSelectPlayer', () => {
+      describe('when onSelectPlayer is called', () => {
+        it('should set the chosen player and their equipable upgrades', () => {
+          const wrapper = shallow(
+            <PlayerLobby
+              FetchPlayer={fetchPlayer}
+              ListUpgradesForPlayer={listUpgrades}
+            />
+          );
+          wrapper.instance().onSelectPlayer('aabb');
+
+          expect(fetchPlayer).toBeCalledWith('aabb');
+          expect(listUpgrades).toBeCalledWith('aabb');
+
+          expect(wrapper).toHaveState({
+            chosenPlayer: { id: 'aabb' },
+            upgrades: ['some-upgrade']
+          });
+        });
+      });
+
+      describe('when PlayersList calls SelectPlayer', () => {
+        it('should set the chosen player', () => {
+          const wrapper = shallow(
+            <PlayerLobby
+              FetchPlayer={fetchPlayer}
+              ListUpgradesForPlayer={listUpgrades}
+            />
+          );
+          const list = wrapper.find(PlayersList);
+          list.props().SelectPlayer('aabb');
+
+          expect(wrapper).toHaveState('chosenPlayer', { id: 'aabb' });
+        });
+      });
+    });
+
+    describe('onEquipUpgrade', () => {
+      let slots;
+
+      beforeEach(() => {
+        slots = [{ id: 1 }];
+        fetchPlayer.mockImplementation(id => ({ id: id, slots: slots }));
+      });
+
+      describe('when onEquipUpgrade is called', () => {
+        it('should update the player and equipable upgrades', () => {
+          const wrapper = shallow(
+            <PlayerLobby
+              ListUpgradesForPlayer={listUpgrades}
+              FetchPlayer={fetchPlayer}
+              UpdatePlayerSlots={updatePlayerSlots}
+            />
+          ).setState({
+            chosenPlayer: { id: 'aabb' }
+          });
+          wrapper.instance().onEquipUpgrade(1);
+
+          expect(updatePlayerSlots).toBeCalledWith('aabb', 1, 'add');
+          expect(fetchPlayer).toBeCalledWith('aabb');
+          expect(listUpgrades).toBeCalledWith('aabb');
+
+          expect(wrapper).toHaveState({
+            chosenPlayer: {
+              id: 'aabb',
+              slots: slots
+            },
+            upgrades: ['some-upgrade']
+          });
+        });
+      });
+
+      describe('when PlayerMenu calls EquipUpgrade', () => {
+        it('should update the player', () => {
+          const wrapper = shallow(
+            <PlayerLobby
+              ListUpgradesForPlayer={listUpgrades}
+              FetchPlayer={fetchPlayer}
+              UpdatePlayerSlots={updatePlayerSlots}
+            />
+          ).setState({
+            chosenPlayer: { id: 'aabb' }
+          });
+          const menu = wrapper.find(PlayerMenu);
+          menu.props().EquipUpgrade(1);
+
+          expect(wrapper).toHaveState('chosenPlayer', {
+            id: 'aabb',
+            slots: slots
+          });
+        });
+      });
+    });
+
+    describe('onPurchaseUpgrade', () => {
+      let slots;
+
+      beforeEach(() => {
+        slots = [{ id: 1 }];
+        fetchPlayer.mockImplementation(id => ({ id: id, slots: slots }));
+      });
+
+      describe('when onPurchaseUpgrade is called', () => {
+        it('should update the player', () => {
+          const wrapper = shallow(
+            <PlayerLobby
+              ListUpgradesForPlayer={listUpgrades}
+              FetchPlayer={fetchPlayer}
+              UpdatePlayerHangarUpgrades={updatePlayerHangarUpgrades}
+            />
+          ).setState({
+            chosenPlayer: { id: 'aabb' }
+          });
+          wrapper.instance().onPurchaseUpgrade(1);
+
+          expect(updatePlayerHangarUpgrades).toBeCalledWith('aabb', 1, 'add');
+          expect(fetchPlayer).toBeCalledWith('aabb');
+          expect(listUpgrades).toBeCalledWith('aabb');
+          expect(wrapper).toHaveState({
+            chosenPlayer: {
+              id: 'aabb',
+              slots: slots
+            },
+            upgrades: ['some-upgrade']
+          });
+        });
+      });
+
+      describe('when PlayerMenu calls PurchaseUpgrade', () => {
+        it('should update the player', () => {
+          const wrapper = shallow(
+            <PlayerLobby
+              ListUpgradesForPlayer={listUpgrades}
+              FetchPlayer={fetchPlayer}
+              UpdatePlayerHangarUpgrades={updatePlayerHangarUpgrades}
+            />
+          ).setState({
+            chosenPlayer: { id: 'aabb' }
+          });
+          const menu = wrapper.find(PlayerMenu);
+          menu.props().PurchaseUpgrade(1);
+
+          expect(wrapper).toHaveState('chosenPlayer', {
+            id: 'aabb',
+            slots: slots
+          });
+        });
+      });
+    });
+
+    describe('onUnequipUpgrade', () => {
+      let slots;
+
+      beforeEach(() => {
+        slots = [{ id: 1 }];
+        fetchPlayer.mockImplementation(id => ({ id: id, slots: slots }));
+      });
+
+      describe('when onUnequipUpgrade is called', () => {
+        it('should update the player', () => {
+          const wrapper = shallow(
+            <PlayerLobby
+              ListUpgradesForPlayer={listUpgrades}
+              FetchPlayer={fetchPlayer}
+              UpdatePlayerSlots={updatePlayerSlots}
+            />
+          ).setState({
+            chosenPlayer: { id: 'aabb' }
+          });
+          wrapper.instance().onUnequipUpgrade(1);
+
+          expect(updatePlayerSlots).toBeCalledWith('aabb', 1, 'remove');
+          expect(fetchPlayer).toBeCalledWith('aabb');
+          expect(listUpgrades).toBeCalledWith('aabb');
+          expect(wrapper).toHaveState({
+            chosenPlayer: {
+              id: 'aabb',
+              slots: slots
+            },
+            upgrades: ['some-upgrade']
+          });
+        });
+      });
+
+      describe('when PlayerMenu calls UnequipUpgrade', () => {
+        it('should update the player', () => {
+          const wrapper = shallow(
+            <PlayerLobby
+              ListUpgradesForPlayer={listUpgrades}
+              FetchPlayer={fetchPlayer}
+              UpdatePlayerSlots={updatePlayerSlots}
+            />
+          ).setState({
+            chosenPlayer: { id: 'aabb' }
+          });
+          const menu = wrapper.find(PlayerMenu);
+          menu.props().UnequipUpgrade(1);
+
+          expect(wrapper).toHaveState('chosenPlayer', {
+            id: 'aabb',
+            slots: slots
+          });
+        });
+      });
+    });
+
+    describe('onCreatePlayer', () => {
+      describe('when onCreatePlayer is called', () => {
+        it('sets calls CreatePlayer', () => {
+          const wrapper = shallow(<PlayerLobby CreatePlayer={createPlayer} />);
+          const id = wrapper.instance().onCreatePlayer({ a: 'b' });
+
+          expect(createPlayer).toBeCalledWith({ a: 'b' });
+        });
+      });
+
+      describe('when PlayerForm calls CreatePlayer', () => {
+        it('sets calls CreatePlayer', () => {
+          const wrapper = shallow(
+            <PlayerLobby CreatePlayer={createPlayer} />
+          ).setState({
+            isCreateOpen: true
+          });
+          const form = wrapper.find(PlayerForm);
+          form.props().CreatePlayer({ a: 'b' });
+
+          expect(createPlayer).toBeCalledWith({ a: 'b' });
+        });
+      });
+    });
+
+    describe('onCancelCreate', () => {
+      describe('when onCancelCreate is called', () => {
+        it('sets isCreateOpen to false', () => {
+          const wrapper = shallow(<PlayerLobby />).setState({
+            isCreateOpen: true
+          });
+          wrapper.instance().onCancelCreate();
+
+          expect(wrapper).toHaveState('isCreateOpen', false);
+        });
+      });
+
+      describe('when PlayerForm calls onCancelCreate', () => {
+        it('sets isCreateOpen to false', () => {
+          const wrapper = shallow(<PlayerLobby />).setState({
+            isCreateOpen: true
+          });
+          const form = wrapper.find(PlayerForm);
+          form.props().CancelForm();
+
+          expect(wrapper).toHaveState('isCreateOpen', false);
+        });
+      });
+    });
+
+    describe('onNewPlayer', () => {
+      describe('when onNewPlayer is called', () => {
+        it('sets isCreateOpen to true', () => {
+          const wrapper = shallow(<PlayerLobby />);
+          wrapper.instance().onNewPlayer();
+
+          expect(wrapper).toHaveState('isCreateOpen', true);
+        });
+      });
+
+      describe('when PlayerList calls NewPlayer', () => {
+        it('sets isCreateOpen to true', () => {
+          const wrapper = shallow(<PlayerLobby />);
+          const list = wrapper.find(PlayersList);
+          list.props().NewPlayer();
+
+          expect(wrapper).toHaveState('isCreateOpen', true);
+        });
+      });
+    });
+  });
 });
